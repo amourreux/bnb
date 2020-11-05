a4ce122dd837badb957149d103992611e41d5f59
Mehmet Emre <mehmetemrep@gmail.com>
2020-11-05 16:41:20 +0300

    end of section 31 factory and seed stuff

---
+++ database/factories/BookableFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Bookable;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
+
+class BookableFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Bookable::class;
+ 
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $suffix = [
+            'Villa',
+            'House',
+            'Cottage',
+            'Luxury Villas',
+            'Cheap House',
+            'Rooms',
+            'Cheap Rooms',
+            'Luxury Rooms',
+            'Fancy Rooms'
+        ];
+
+        $dude = Arr::random($suffix);
+        //Log::info($dude);
+
+        return [
+            //
+            'title' => $this->faker->city.' '.$dude,
+            'description' => $this->faker->text()
+        ];
+    }
+}
--- database/seeders/BookablesTableSeeder.php
+++ database/seeders/BookablesTableSeeder.php
@@ -3,19 +3,19 @@
 namespace Database\Seeders;
 
 use App\Models\Bookable;
+use Database\Factories\BookableFactory;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class BookablesTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //
         Bookable::factory()->times(50)->create();
-        //$this->call(BookablesTableSeeder::class);
     }
 }
