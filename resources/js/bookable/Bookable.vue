7d3829b61065642f24e17a113f82d1024fa25684
Mehmet Emre <mehmetemrep@gmail.com>
2020-11-10 13:55:41 +0300

    end of section 5.39

---
+++ resources/js/bookable/Bookable.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        bookable
+    </div>
+</template>
+
+
+<script>
+export default {
+    created() {
+        console.log(this.$route.params.id);
+        const request = axios.get(`/api/bookables/${this.$route.params.id}`);
+        request.then(
+            responce => {
+                console.log(responce.data);
+            }
+        );
+    },
+    data() {
+        return {
+            bookable: null
+        }
+    },
+}
+</script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,18 +1,17 @@
 import VueRouter from "vue-router";
-import ExampleComponent from "./components/ExampleComponent";
-import Example2 from "./components/Example2";
 import Bookables from "./bookables/Bookables";
+import Bookable from "./bookable/Bookable"
 
 const routes = [{
     path: "/",
     component: Bookables,
     name: "home",
-},
-{
-    path: "/second",
-    component: Example2,
-    name: "second",
-}];
+},{
+    path: "/bookable/:id",
+    component: Bookable,
+    name: "bookable"
+}
+];
 
 const router = new VueRouter({
     routes, 
