208b0b914cebe629898379090a692b89b237fc25
Mehmet Emre <mehmetemrep@gmail.com>
2021-02-24 14:36:01 +0300

    end of section 6:55

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bookable;
 use Illuminate\Http\Request;
 
 class BookableAvailabilityController extends Controller
@@ -13,7 +14,7 @@ class BookableAvailabilityController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke($id, Request $request)
     {
         //
         $data = $request->validate([
@@ -21,6 +22,7 @@ class BookableAvailabilityController extends Controller
             'to' => 'required|date_format:Y-m-d|after_or_equal:from'
         ]);
 
-        return $request;
+        $bookable = Bookable::findOrFail($id);
+        dd($bookable->bookings()->betweenDates($data['from'], $data['to']));
     }
 }
