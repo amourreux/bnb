2677c2fbefc283482e4e23c156ca7c7481055b40
Mehmet Emre <mehmetemrep@gmail.com>
2021-03-01 11:19:18 +0300

    end of section 8:70

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,7 @@
 import VueRouter from "vue-router";
 import Bookables from "./bookables/Bookables";
-import Bookable from "./bookable/Bookable"
+import Bookable from "./bookable/Bookable";
+import Review from "./review/Revıew";
 
 const routes = [{
     path: "/",
@@ -10,6 +11,11 @@ const routes = [{
     path: "/bookable/:id",
     component: Bookable,
     name: "bookable"
+}
+,{
+    path: "/review/:id",
+    component: Review,
+    name: "review"
 }
 ];
 
