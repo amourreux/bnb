016deb4f49e20d5748a80d7006850473d7bd0afc
Mehmet Emre <mehmetemrep@gmail.com>
2021-03-03 11:55:50 +0300

    eof section 8:76

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,16 +1,37 @@
 <template>
     <div>
-        <div class="form-group">
-            <label for="" class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
-            <star-rating class="fa-3x" v-model="review.rating"></star-rating>
+        <div v-if="loading">Loading...</div>
+        <div v-else>
+            <div v-if="alreadyReviewed">
+                <h3>You@ve allready left a review for this booking!</h3>
+            </div>
+            <div v-else>
+                <div class="form-group">
+                    <label for="" class="text-muted"
+                        >Select the star rating (1 is worst - 5 is best)</label
+                    >
+                    <star-rating
+                        class="fa-3x"
+                        v-model="review.rating"
+                    ></star-rating>
+                </div>
+                <div class="form-group">
+                    <label for="content" class="text-muted"
+                        >Describe your experience with</label
+                    >
+                    <textarea
+                        name="content"
+                        cols="30"
+                        rows="10"
+                        class="form-control"
+                        v-model="review.content"
+                    ></textarea>
+                </div>
+                <button class="btn-lg btn-primary btn-block">
+                    Submit
+                </button>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="content" class="text-muted">Describe your experience with</label>
-            <textarea name="content" cols="30" rows="10" class="form-control" v-model="revıew.content"></textarea>
-        </div>
-        <button class="btn-lg btn-primary btn-block">
-            Submit
-        </button>
     </div>
 </template>
 
@@ -21,10 +42,25 @@ export default {
             review: {
                 rating: 5,
                 content: null
-            }
+            },
+            existingReview: null,
+            loading: false
+        };
+    },
+    created() {
+        this.loading = true;
+        axios
+            .get(`/api/get/reviews/${this.$route.params.id}`)
+            .then(response => (this.existingReview = response.data.data))
+            .catch((err => {
+                
+            }))
+            .then(() => (this.loading = false));
+    },
+    computed: {
+        alreadyReviewd() {
+            return this.existingReview !== null;
         }
-    },created() {
-        
     }
-}
+};
 </script>
