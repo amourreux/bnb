4561924ccea863f10450fc1ded4e280914573a84
Mehmet Emre <mehmetemrep@gmail.com>
2021-05-04 16:20:10 +0300

    eof seciton 9:100 storing local storage

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -55,7 +55,7 @@ export default {
             this.loading = true;
             this.errors = null;
 
-            this.$store.commit('setLastSearch', {
+            this.$store.dispatch('setLastSearch', {
                 from: this.from,
                 to: this.to
             });
--- resources/js/index.vue
+++ resources/js/index.vue
@@ -13,3 +13,19 @@
         
     </div>
 </template>
+
+<script>
+
+import {mapState} from 'vuex';
+
+export default {
+    data() {
+        return{
+            lastSearch: this.$store.state.lastSearch
+        };
+    },
+    computed : mapState({
+        lastSearchComputed: "lastSearch"
+    })
+}
+</script>
