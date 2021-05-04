e93c0d6caa6a1952455d1741924d56b01c52f5f7
Mehmet Emre <mehmetemrep@gmail.com>
2021-05-04 12:05:39 +0300

    eof section 9:95. Vuex introduction - managing global state

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,17 @@ require('./bootstrap');
 import router from "./routes"
 import VueRouter from "vue-router";
 import index from "./index.vue";
+import Vuex from 'vuex';
 import moment from "moment";
 import StarRating from "./shared/components/StarRating";
 import FatalError from "./shared/components/FatalError";
 import Success from "./shared/components/Success";
 import ValidationErrors from "./shared/components/ValidationErrors";
+import Vue from "vue";
 
 window.Vue = require('vue');
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 Vue.filter("fromNow", value => moment(value).fromNow());
 Vue.component("star-rating", StarRating);
