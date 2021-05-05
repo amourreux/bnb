bbe2a407ce9891f5c75017ce2ad6b606455abc3f
Mehmet Emre <mehmetemrep@gmail.com>
2021-05-05 10:15:19 +0300

    eof section 9:101 Vuex

--- resources/js/index.vue
+++ resources/js/index.vue
@@ -1,31 +1,32 @@
 <template>
     <div>
         <nav class="navbar bg-white border-bottom navbar-light">
-            <router-link class="navbar-brand mr-auto" v-bind:to="{ name: 'home' } ">LaravelBnb</router-link>
+            <router-link
+                class="navbar-brand mr-auto"
+                v-bind:to="{ name: 'home' }"
+                >LaravelBnb</router-link
+            >
         </nav>
 
         <div class="container mt-4 mb-4 pr-4 pl-4">
-
             <router-view></router-view>
-
         </div>
-
-        
     </div>
 </template>
 
 <script>
-
-import {mapState} from 'vuex';
+import { mapState } from "vuex";
 
 export default {
     data() {
-        return{
+        return {
             lastSearch: this.$store.state.lastSearch
         };
     },
-    computed : mapState({
-        lastSearchComputed: "lastSearch"
-    })
-}
+    computed: {
+        ...mapState({
+            lastSearchComputed: "lastSearch"
+        })
+    }
+};
 </script>
