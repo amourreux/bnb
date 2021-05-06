f756ecbb0df322ca2a185e96c6e8ec2669c61676
Mehmet Emre <mehmetemrep@gmail.com>
2021-05-06 12:59:03 +0300

    eof seciton 10:102. Vuew Transitions

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -2,34 +2,50 @@
     <div>
         <h6 class="text-uppercase text-secondary font-weight-border">
             Check Availability
-            <span v-if="noAvailability" class="text-danger">(NOT AVAILABLE)</span>
-            <span v-if="hasAvailability" class="text-success">(AVAILABLE)</span>
+
+            <transition name="fade">
+                <span v-if="noAvailability" class="text-danger">(NOT AVAILABLE)</span>
+                <span v-if="hasAvailability" class="text-success">(AVAILABLE)</span>
+            </transition>
         </h6>
         <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="from">From</label>
-                    <input type="text"
+            <div class="form-group col-md-6">
+                <label for="from">From</label>
+                <input
+                    type="text"
                     name="from"
-                    class="form-control form-control-sm" 
-                    placeholder="Start Date" 
+                    class="form-control form-control-sm"
+                    placeholder="Start Date"
                     v-model="from"
                     @keyup.enter="check"
-                    :class="[{'is-invalid' : errorFor('from') }]">
-                    
-                    <v-errors :errors="this.errorFor('from')"></v-errors>
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="to">To</label>
-                    <input type="text" name="to" class="form-control form-control-sm" placeholder="End Date" v-model="to"
+                    :class="[{ 'is-invalid': errorFor('from') }]"
+                />
+
+                <v-errors :errors="this.errorFor('from')"></v-errors>
+            </div>
+            <div class="form-group col-md-6">
+                <label for="to">To</label>
+                <input
+                    type="text"
+                    name="to"
+                    class="form-control form-control-sm"
+                    placeholder="End Date"
+                    v-model="to"
                     @keyup.enter="check"
-                    :class="[{'is-invalid' : this.errorFor('to')}]">
-                    
-                    <v-errors :errors="errorFor('to')"></v-errors>
-                </div>
-                
+                    :class="[{ 'is-invalid': this.errorFor('to') }]"
+                />
+
+                <v-errors :errors="errorFor('to')"></v-errors>
+            </div>
         </div>
 
-        <button class="btn btn-secondary btn-block" @click="check" :disabled="loading">Check!</button>
+        <button
+            class="btn btn-secondary btn-block"
+            @click="check"
+            :disabled="loading"
+        >
+            Check!
+        </button>
     </div>
 </template>
 
@@ -40,64 +56,69 @@ import validationErrors from "./../shared/mixins/validationErrors";
 export default {
     mixins: [validationErrors],
     props: {
-        bookableId: [String, Number],
+        bookableId: [String, Number]
     },
     data() {
         return {
             from: this.$store.state.lastSearch.from,
             to: this.$store.state.lastSearch.to,
             loading: false,
-            status: null,
+            status: null
         };
     },
-    methods : {
+    methods: {
         check() {
             this.loading = true;
             this.errors = null;
 
-            this.$store.dispatch('setLastSearch', {
+            this.$store.dispatch("setLastSearch", {
                 from: this.from,
                 to: this.to
             });
 
-            axios.get(`/api/bookables/${this.bookableId}/availability?from=${this.from}&to=${this.to}`).then(response => {
-                this.status = response.status;
-            }).catch(error => {
-                if(is422(error)){
-                    this.errors = error.response.data.errors;
-                }
-                this.status = error.response.status;
-            }).then(() => this.loading = false);
+            axios
+                .get(
+                    `/api/bookables/${this.bookableId}/availability?from=${this.from}&to=${this.to}`
+                )
+                .then(response => {
+                    this.status = response.status;
+                })
+                .catch(error => {
+                    if (is422(error)) {
+                        this.errors = error.response.data.errors;
+                    }
+                    this.status = error.response.status;
+                })
+                .then(() => (this.loading = false));
         }
     },
     computed: {
         hasErrors() {
             return 422 === this.status && this.errors !== null;
         },
-        hasAvailability(){
+        hasAvailability() {
             return 200 === this.status;
         },
         noAvailability() {
             return 404 === this.status;
         }
     }
-}
+};
 </script>
 
-
 <style scoped>
-    label {
-        font-size: 0.7rem;
-        text-transform: uppercase;
-        color: gray;
-        font-weight: bolder;
-    }
-    .is-invalid{
-        border-color: #b22222;
-        background-image: none;
-    }
+label {
+    font-size: 0.7rem;
+    text-transform: uppercase;
+    color: gray;
+    font-weight: bolder;
+}
+.is-invalid {
+    border-color: #b22222;
+    background-image: none;
+}
 
-    .invalid-feedback{
-        border-color: #b22222;
-    }
+.invalid-feedback {
+    border-color: #b22222;
+}
 </style>
